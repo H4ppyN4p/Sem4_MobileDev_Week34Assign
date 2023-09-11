26e8805242e79cb439c03ae6fd0ac5992f1452d5
H4ppyN4p <tobi5703@stud.kea.dk>
2023-09-11 15:07:29 +0200

    Modified the source files to update name too

--- App.js
+++ App.js
@@ -1,14 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState } from 'react';
+
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+
+function HomePage({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <View style={styles.buttonView}>
+        <Button style={styles.buttons}
+        title='Go to About page'
+        onPress={() =>
+        navigation.navigate('About')}
+        />
+      </View>
+
+      <View style={styles.buttonView}>
+        <Button style={styles.buttons} 
+        title='Go to "Read More"'
+        onPress={() =>  
+        navigation.navigate('ReadMore')}
+        />
+      </View>
+
+    <Text style={styles.text}>This is the home page</Text>
+    
+</View>
   );
 }
+
+function AboutPage() {
+  return (
+    <View>
+      <Text>This is the AboutMe section</Text>
+    </View>
+  )
+}
+
+function ReadMore() {
+  return (
+    <View>
+      <Text>This is the Read More section</Text>
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name="Home" component={HomePage} />
+
+        <Stack.Screen name="About" component={AboutPage} />
+
+        <Stack.Screen name="ReadMore" component={ReadMore} />
+
+
+
+      </Stack.Navigator>  
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +78,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonView: {
+    marginTop: "10px"
+  }
 });
